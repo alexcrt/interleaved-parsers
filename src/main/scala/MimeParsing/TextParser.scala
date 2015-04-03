c263634812d667ba237c9ea045ef25dd1dd52b14
alexcrt <alexis.cartier@gmail.com>
2015-04-03 23:33:41 +0200

    takeUntil takes the bound

--- src/main/scala/MimeParsing/TextParser.scala
+++ src/main/scala/MimeParsing/TextParser.scala
@@ -12,7 +12,7 @@ object TextParser extends RegexParsers {
     def takeUntil(condition: Parser[String], parser: Parser[String]): Parser[List[String]] = rep(not(condition) ~> parser)
 
     def root(boundary: Option[String] = None): Parser[List[String]] = boundary match {
-        case Some(bound) => takeUntil("--frontier", TEXTDATA <~ CRLF)
+        case Some(bound) => takeUntil(bound, TEXTDATA <~ CRLF)
         case None => repsep(TEXTDATA, CRLF)
     }
   
