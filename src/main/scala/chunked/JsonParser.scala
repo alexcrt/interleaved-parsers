befe00ac724589aba274edaf1f46af4b12790df2
alexcrt <alexis.cartier@gmail.com>
2015-05-25 15:33:00 +0200

    bench results

--- src/main/scala/chunked/JsonParser.scala
+++ src/main/scala/chunked/JsonParser.scala
@@ -24,7 +24,5 @@ object JsonParser extends StringParsers {
     def obj: Parser[Map[String, Any]] = "{" ~> w ~> repsep(member, w ~>","<~ w) <~ w <~"}" ^^ (ListMap() ++ _)
 
     def arr: Parser[List[Any]] = "["~> w ~> repsep(value, w ~>","<~ w) <~ w <~"]"
-
-    def member: Parser[(String, Any)] = stringLiteral ~ w ~ ":" ~ w ~ value map {case name ~ w1 ~ ":" ~ w2 ~ value => (name, value)}
-  def member2: Parser[(String, Any)] = (stringLiteral <~ w <~ ":" ) ~ (w ~> value) map {case name ~ value => (name, value)}
+    def member: Parser[(String, Any)] = (stringLiteral <~ w <~ ":" ) ~ (w ~> value) map {case name ~ value => (name, value)}
 }
